cc018416ff28e55b949c0a35f479c28e89938139
John Gallagher <jmg@cs.columbia.edu>
2011-03-23 02:03:12 -0400

    tested on Motorola Droid (note: audio is bad on emulator, but fine on real HW)  New: X axis is AM modulator. Y Axis is A to A scale

--- src/edu/columbia/cs/jmg/ToneDef/ToneDef.java
+++ src/edu/columbia/cs/jmg/ToneDef/ToneDef.java
@@ -2,6 +2,7 @@ package edu.columbia.cs.jmg.ToneDef;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.*;
@@ -50,16 +51,50 @@ public class ToneDef extends Activity {
         findViewById(R.id.tapAreaView).setOnTouchListener(new View.OnTouchListener() {
 			
 			public boolean onTouch(View v, MotionEvent me) {
-				double numQuants = 13.0;
-				double quantSize = v.getHeight()/numQuants;
+				final double numQuants = 13.0;
+				final double quantSize = v.getHeight()/numQuants;
+				final double quantSizeX = v.getWidth()/numQuants;
 				int quant = (int)(me.getY()/quantSize);
-				
-				int freq = (int)(440.0 * Math.pow(2, (quant)/(numQuants)));
-				setStatus("Freq: "+freq+" note: "+quant);
+				int quantX = (int)(me.getX()/quantSizeX);
+				int freq = indexToFreq(440.0, quant);
+				int freqM = 5 + quantX * 50;
+				setStatus("Freq: "+freq+" note: "+quant+" Modulator: "+freqM);
 				stream.add(freq);
+				stream.addModulator(freqM);
 				return true; //more events from this stroke are wanted
 			}
         });
+        
+        new Thread(new Runnable()
+        {
+        	public void run()
+        	{
+        		long statsCounter = 0;
+				long statsNumerator = 0; 
+        		int[] indices = {0, 4, 7, 11, 7, 4, 4, 7, 11, 14, 11, 7, 0, 4, 7, 11, 7, 4, 5, 9, 12, 16, 9, 5, 0, 4, 7, 11, 7, 4, 2, 5, 9, 12, 9, 5, 5, 9, 12, 16, 9, 5, 0, 4, 7, 11, 7, 4, 0};
+        		while(true)
+        		for(int i = 0; i<indices.length; i++)
+        		{
+        			
+        			long start = System.nanoTime();
+        			stream.add(indexToFreq(440.0, indices[i]));
+        			try {
+						Thread.sleep(500);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						break;
+					}
+					long elapsed = (System.nanoTime() - start)/1000000;
+					statsNumerator += elapsed;
+					statsCounter++;
+					if(statsCounter % 2 == 0)
+					{
+						Log.d("ToneDef","Loopstats: "+(statsNumerator/statsCounter)+" msec wait.");
+					}
+					
+        		}
+        	}
+        });//.start();
         
 		
         
@@ -68,5 +103,10 @@ public class ToneDef extends Activity {
     private void setStatus(String statusText)
     {
     	status.setText(statusText);
+    }
+    
+    private static int indexToFreq(double base, int index)
+    {
+		return (int)(base * Math.pow(2, (index)/(12.0)));
     }
 }
--- src/edu/columbia/cs/jmg/ToneDef/ToneStream.java
+++ src/edu/columbia/cs/jmg/ToneDef/ToneStream.java
@@ -1,114 +1,173 @@
 package edu.columbia.cs.jmg.ToneDef;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import android.util.Log;
+
 
 
 public class ToneStream
 {
-	Thread loop;
+	final int N_BUFFERS = 2;
+	final int BUFSIZE = 1024;
+	Thread loop, shaper, sender;
 	boolean started;
-	//final LinkedBlockingQueue<Integer> toneq; 
-
 	volatile Integer tone;
+	volatile Float increment;
+	volatile Float modulatorIncrement;
+	
+	float[][] buffers = new float[N_BUFFERS][BUFSIZE];
+	ConcurrentLinkedQueue<float[]> shapeq, sendq;
+	
+	boolean stopflag;
 	
 	
 	public ToneStream()
 	{
 		started = false;
 		tone = 0;
+		increment = 0.0f;
+		modulatorIncrement = 0.0f;
+		shapeq = new ConcurrentLinkedQueue<float[]>();
+		sendq = new ConcurrentLinkedQueue<float[]>();
+		
 	}
 	
 	public void start()
 	{
 		tone = 0;
-		loop = new Thread(new Runnable()
+		
+		//move all buffers to the shaper
+		sendq.clear();
+		for(int i = 0; i < buffers.length; i++)
+			shapeq.add(buffers[i]);
+		
+		started = true;
+		
+		shaper = new Thread(new Runnable()
 		{
 			public void run()
 			{
-				AndroidAudioDevice device = new AndroidAudioDevice();
-				float freq = 0;
-				float increment = 0;
-				float angle = 0;
-				float samples[] = new float[1024];
+				float[] activeBuffer = null;
+				float angle = 0, modulatorAngle = 0, wavtmp, modtmp;
 				
-				
-				while (true) 
+				while(true)
 				{
-					Integer headfreq = null;
-					/*
-					if(freq > 0)  //if there is something playing, do non-blocking version
+					if(!started)
 					{
-						headfreq = toneq.poll();
-						
+						break;
 					}
-					else //else do blocking version
+					while((activeBuffer = shapeq.poll()) == null)
 					{
+						if(!started)
+						{
+							return;
+						}
 						try 
 						{
-							headfreq = toneq.take();
-						} 
+							//shapeq.wait();
+							Thread.sleep(5);
+						}
 						catch (InterruptedException e) 
 						{
-							break;
-						}
-					}*/
-					headfreq = tone;
-					
-					if(headfreq != null)
-					{
-						freq = headfreq;
-						increment = getIncrement(freq);
-					}
-					
-					if(freq > 0)
-					{
-					
-						for (int i = 0; i < samples.length; i++) {
-							samples[i] = (float) Math.sin(angle);
-							angle += increment;
-						}
-		
-						device.writeSamples(samples);
-					}
-					else
-					{
-						try 
-						{
-							Thread.sleep(100);
-						} 
-						catch (InterruptedException e) 
-						{
-							break;
+							return;
 						}
 					}
+					//Log.d("ToneDef","Shaper polled. "+shapeq.size());
+					for (int i = 0; i < activeBuffer.length; i++) 
+					{
+						wavtmp = (float) Math.sin(angle);
+						angle += increment;
+						modtmp = (float) Math.sin(modulatorAngle) * 0.5f;
+						modulatorAngle += modulatorIncrement;
+						activeBuffer[i] = wavtmp * modtmp;
+					}
+					if(angle > 10)
+						angle = angle - (int)(angle/(2*Math.PI)) * (float)(2*Math.PI);
+					if(modulatorAngle > 10)
+						modulatorAngle = modulatorAngle - (int)(modulatorAngle/(2*Math.PI)) * (float)(2*Math.PI);
+					sendq.add(activeBuffer);
+					//Log.d("ToneDef","shaper added to sender. "+sendq.size());
 					
-					if(freq < 0)
+					//sendq.notify();
+					activeBuffer = null;
+					
+				}
+			}
+		});
+		sender = new Thread(new Runnable()
+		{
+			public void run()
+			{
+				float[] activeBuffer = null;
+				AndroidAudioDevice device = new AndroidAudioDevice();
+				
+				while(true)
+				{
+					if(!started)
 					{
 						device.stop();
 						break;
 					}
+					//Log.d("ToneDef","Sender init polling. "+sendq.size());
+					while((activeBuffer = sendq.poll()) == null)
+					{
+						//Log.d("ToneDef","Sender polling. "+sendq.size());
+						if(!started)
+						{
+							device.stop();
+							return;
+						}
+						try 
+						{
+							Thread.sleep(1);
+						}
+						catch (InterruptedException e) 
+						{
+							return;
+						}
+						
+					}
+					//broke out of the loop
+					//Log.d("ToneDef","Sender polled. "+sendq.size());
+					device.writeSamples(activeBuffer);
+					shapeq.add(activeBuffer);
+					//Log.d("ToneDef","sender added to shaper. ");
+					//shapeq.notify();
+					activeBuffer = null;
 				}
+				
 			}
 		});
-		loop.start();
-		started = true;
+		sender.start();
+		shaper.start();
 	}
 	
 	public void stop()
 	{
 		tone = -1;
+		started = false;
 		try {
 			loop.join();
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		started = false;
+		
 	}
 	
 	public void add(int freq)
 	{
 		//toneq.add(freq);
 		tone = freq;
+		increment = getIncrement(freq);
+	}
+	
+	public void addModulator(int freq)
+	{
+		//toneq.add(freq);
+		//tone = freq;
+		modulatorIncrement = getIncrement(freq);
 	}
 	
 	public boolean isStarted()
